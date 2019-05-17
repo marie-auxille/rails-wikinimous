c6511e831984612cc1e35bc337de2156d277076d
marieauxille denis <marieauxille@gmail.com>
2019-05-17 17:25:00 +0200

    seed populated

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,23 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def delete
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+10.times do
+Article.create(
+  title: Faker::Lorem.sentence,
+  content: Faker::Lorem.paragraph(1))
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
